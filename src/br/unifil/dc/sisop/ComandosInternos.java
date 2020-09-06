dd38f62234ad31370d68e7dfc6af8d8680039783
Leonardo Kawasaki <leonardokws@gmail.com>
2020-09-06 16:47:22 -0300

    Primeiro commit

---
+++ src/br/unifil/dc/sisop/ComandosInternos.java
@@ -0,0 +1,36 @@
+package br.unifil.dc.sisop;
+
+import java.util.Optional;
+
+/**
+ * Write a description of class ComandosInternos here.
+ *
+ * @author Ricardo Inacio Alvares e Silva
+ * @version 180823
+ */
+public final class ComandosInternos {
+    /**
+     * Essa classe não deve ser instanciada.
+     */
+    private ComandosInternos() {}
+
+    public static int exibirRelogio() {
+        throw new RuntimeException("Método ainda não implementado");
+    }
+
+    public static int escreverListaArquivos(Optional<String> nomeDir) {
+        throw new RuntimeException("Método ainda não implementado");
+    }
+
+    public static int criarNovoDiretorio(String nomeDir) {
+        throw new RuntimeException("Método ainda não implementado");
+    }
+
+    public static int apagarDiretorio(String nomeDir) {
+        throw new RuntimeException("Método ainda não implementado");
+    }
+
+    public static int mudarDiretorioTrabalho(String nomeDir){
+        throw new RuntimeException("Método ainda não implementado");
+    }
+}
